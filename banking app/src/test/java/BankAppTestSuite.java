e7b617d65c6ead4429b48489d2dfb38e5c85622d
Amit Patil <117425578+Asteriskkkk@users.noreply.github.com>
2026-07-27 18:45:06 +0530

    removed

--- banking app/src/test/java/BankAppTestSuite.java
+++
@@ -1,9 +0,0 @@
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
-
-@Suite
-@SuiteDisplayName("Bank App Test Suite")
-@SelectClasses({BankAccountTest.class, TransactionServiceTest.class, NotificationServiceTest.class})
-class BankAppTestSuite {
-}
